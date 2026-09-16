c2e9fdb199efb189eb2954e3f8508be230daff99
P2wn <racecarboy77@gmail.com>
2026-09-16 16:49:35 -0400

    fix(bedrock): wire redesigned guild settings dependencies

--- src/main/kotlin/net/lumalyte/lg/interaction/menus/bedrock/BedrockGuildSettingsMenu.kt
+++ src/main/kotlin/net/lumalyte/lg/interaction/menus/bedrock/BedrockGuildSettingsMenu.kt
@@ -1,10 +1,17 @@
 package net.lumalyte.lg.interaction.menus.bedrock
 
+import net.lumalyte.lg.application.persistence.ProgressionRepository
+import net.lumalyte.lg.application.services.ConfigService
+import net.lumalyte.lg.application.services.GuildService
+import net.lumalyte.lg.application.services.ProgressionService
 import net.lumalyte.lg.domain.entities.Guild
 import net.lumalyte.lg.interaction.menus.Menu
+import net.lumalyte.lg.interaction.menus.MenuFactory
 import net.lumalyte.lg.interaction.menus.MenuNavigator
 import net.lumalyte.lg.interaction.menus.guild.GuildSettingsMenu
+import net.lumalyte.lg.utils.MenuItemBuilder
 import org.bukkit.entity.Player
+import org.koin.core.context.GlobalContext
 import java.util.logging.Logger
 
 /** Bedrock follows the same redesigned settings hierarchy as Java. */
@@ -15,6 +22,17 @@ class BedrockGuildSettingsMenu(
     @Suppress("UNUSED_PARAMETER") private val logger: Logger,
 ) : Menu {
     override fun open() {
-        GuildSettingsMenu(menuNavigator, player, guild).open()
+        val koin = GlobalContext.get()
+        GuildSettingsMenu(
+            menuNavigator = menuNavigator,
+            player = player,
+            guild = guild,
+            guildService = koin.get<GuildService>(),
+            menuItemBuilder = koin.get<MenuItemBuilder>(),
+            menuFactory = koin.get<MenuFactory>(),
+            configService = koin.get<ConfigService>(),
+            progressionService = koin.get<ProgressionService>(),
+            progressionRepository = koin.get<ProgressionRepository>(),
+        ).open()
     }
 }
